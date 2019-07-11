c383e24a5f1bbf1b7cc2f2d267685b56681011e8
Andrey Tabolin <tabolin_andrey@mail.ru>
2019-07-11 09:24:56 +0300

    0.2.1: add yaml tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -21,3 +21,24 @@ test('compare two flat json files, both are equal', () => {
   const result = fs.readFileSync(`${__dirname}/__fixtures__/0.1.x/test3/result.txt`, 'utf8');
   expect(gendiff(before, after)).toBe(result);
 });
+
+test('compare two flat yaml files, v.1', () => {
+  const before = `${__dirname}/__fixtures__/0.2.x/test1/before.yml`;
+  const after = `${__dirname}/__fixtures__/0.2.x/test1/after.yml`;
+  const result = fs.readFileSync(`${__dirname}/__fixtures__/0.2.x/test1/result.txt`, 'utf8');
+  expect(gendiff(before, after)).toBe(result);
+});
+
+test('compare two flat yaml files, v.2', () => {
+  const before = `${__dirname}/__fixtures__/0.2.x/test2/before.yml`;
+  const after = `${__dirname}/__fixtures__/0.2.x/test2/after.yml`;
+  const result = fs.readFileSync(`${__dirname}/__fixtures__/0.2.x/test2/result.txt`, 'utf8');
+  expect(gendiff(before, after)).toBe(result);
+});
+
+test('compare two flat yaml files, both are equal', () => {
+  const before = `${__dirname}/__fixtures__/0.2.x/test3/before.yml`;
+  const after = `${__dirname}/__fixtures__/0.2.x/test3/after.yml`;
+  const result = fs.readFileSync(`${__dirname}/__fixtures__/0.2.x/test3/result.txt`, 'utf8');
+  expect(gendiff(before, after)).toBe(result);
+});
