39b5ee91b30116be1ff2b14ef380a4b2ab836084
Andrey Tabolin <tabolin_andrey@mail.ru>
2019-08-02 08:27:33 +0300

    removed json file formatter

--- src/render.js
+++ src/render.js
@@ -1,11 +1,10 @@
 import plain from './formatters/plain';
-import json from './formatters/json';
 import pretty from './formatters/pretty';
 
 const renders = {
   pretty,
   plain,
-  json,
+  json: JSON.stringify,
 };
 
 export const defineRender = (name, func) => {
