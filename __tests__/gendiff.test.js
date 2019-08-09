d101ac04e55a8ac13ef4b2ccdab10967644dbd73
Andrey Tabolin <tabolin_andrey@mail.ru>
2019-08-09 05:46:00 +0300

    refactored test file

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,70 +1,23 @@
-/* eslint no-undef: 0 */
-
 import fs from 'fs';
-import buildConfigsDiff from '../src/ast';
-import parseConfigToJSON from '../src/parsers';
-import gendiff, { readConfig } from '../src/index';
+import gendiff from '../src';
 
-const getJSONFilesPaths = () => (
+const formats = ['pretty', 'plain', 'json'];
+
+const getFilesPaths = dataType => (
   [
-    `${__dirname}/__fixtures__/json/before.json`,
-    `${__dirname}/__fixtures__/json/after.json`,
-  ]);
+    `${__dirname}/__fixtures__/${dataType}/before.${dataType}`,
+    `${__dirname}/__fixtures__/${dataType}/after.${dataType}`,
+  ]
+);
 
-const getYAMLFilesPaths = () => (
-  [
-    `${__dirname}/__fixtures__/yaml/before.yml`,
-    `${__dirname}/__fixtures__/yaml/after.yml`,
-  ]);
+const getExpectedText = format => fs.readFileSync(`${__dirname}/__fixtures__/outputResults/${format}.txt`, 'utf8');
 
-const getINIFilesPaths = () => (
-  [
-    `${__dirname}/__fixtures__/ini/before.ini`,
-    `${__dirname}/__fixtures__/ini/after.ini`,
-  ]);
-
-const getExpectedAST = () => JSON.parse(fs.readFileSync(`${__dirname}/__fixtures__/outputResults/ast.json`, 'utf8'));
-const getExpectedPrettyText = () => fs.readFileSync(`${__dirname}/__fixtures__/outputResults/pretty.txt`, 'utf8');
-const getExpectedPlainText = () => fs.readFileSync(`${__dirname}/__fixtures__/outputResults/plain.txt`, 'utf8');
-const getExpectedJSONText = () => fs.readFileSync(`${__dirname}/__fixtures__/outputResults/json.txt`, 'utf8');
-
-test.each([
-  getJSONFilesPaths(),
-  getYAMLFilesPaths(),
-  getINIFilesPaths(),
-])('Output = AST\nFiles:\n> %s\n> %s\n---\n', (before, after) => (
-  expect(buildConfigsDiff(parseConfigToJSON(readConfig(before).body, readConfig(before).type),
-    parseConfigToJSON(readConfig(after).body, readConfig(after).type))).toEqual(getExpectedAST())
-));
-
-test.each([
-  getJSONFilesPaths(),
-  getYAMLFilesPaths(),
-  getINIFilesPaths(),
-])('Output = pretty\nFiles:\n> %s\n> %s\n---\n', (before, after) => (
-  expect(gendiff(before, after, 'pretty')).toEqual(getExpectedPrettyText())
-));
-
-test.each([
-  getJSONFilesPaths(),
-  getYAMLFilesPaths(),
-  getINIFilesPaths(),
-])('Output = plain\nFiles:\n> %s\n> %s\n---\n', (before, after) => (
-  expect(gendiff(before, after, 'plain')).toEqual(getExpectedPlainText())
-));
-
-test.each([
-  getJSONFilesPaths(),
-  getYAMLFilesPaths(),
-  getINIFilesPaths(),
-])('Output = json\nFiles:\n> %s\n> %s\n---\n', (before, after) => (
-  expect(gendiff(before, after, 'json')).toEqual(getExpectedJSONText())
-));
-
-test.each([
-  getJSONFilesPaths(),
-  getYAMLFilesPaths(),
-  getINIFilesPaths(),
-])('Format = Object. Output = json\nFiles:\n> %s\n> %s\n---\n', (before, after) => (
-  expect(gendiff(before, after, { format: 'json' })).toEqual(getExpectedJSONText())
+formats.forEach(format => (
+  test.each([
+    getFilesPaths('json'),
+    getFilesPaths('yml'),
+    getFilesPaths('ini'),
+  ])(`Output = ${format}\nFiles:\n> %s\n> %s\n---\n`, (before, after) => (
+    expect(gendiff(before, after, format)).toEqual(getExpectedText(format))
+  ))
 ));
